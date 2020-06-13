3181e6e185e16bd5276b3a61da0b1d5e805a847b
Meh <zavyalovartem79@gmail.com>
2020-06-13 11:13:19 +0500

    Trying to deploy bot with polling

--- MainBot.py
+++ MainBot.py
@@ -271,16 +271,18 @@ def handle_task(message):
         prev_message.text = "Transition"
 
 
-@server.route('/' + config.Token, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+# @server.route('/' + config.Token, methods=['POST'])
+# def getMessage():
+#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+#     return "!", 200
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://divinely-inspired-project.herokuapp.com/' + config.Token)
+#     return "!", 200
+#
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://divinely-inspired-project.herokuapp.com/' + config.Token)
-    return "!", 200
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
+bot.polling(none_stop=True)
