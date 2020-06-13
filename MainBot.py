c4119c6a73c47aeeaf1f3614a3b88a9384e03cf2
Meh <zavyalovartem79@gmail.com>
2020-06-13 18:55:46 +0500

    Fixed bug, working for multiple players, not refactored version

--- MainBot.py
+++ MainBot.py
@@ -21,68 +21,69 @@ from flask import Flask, request
 bot = telebot.TeleBot(config.Token)
 global players
 global Started
-Started = False
 players = {}
+current_handlers = {}
+handlers_on_hold = {}
+prev_types = {}
+prev_parts = {}
+prev_messages = {}
+not_advancing_flags = {}
 global current_nandler
 global handler_on_hold
 global prev_type
 global prev_part
 global prev_message
 global not_advancing
-not_advancing = False
 server = Flask(__name__)
 
 @bot.message_handler(func=lambda message: check_player_in_dict(message.chat.id, "Theory"), content_types=['text'])
 def handle_theory(message):
-    global current_nandler
-    global prev_part
-    global prev_type
-    global prev_message
-    global handler_on_hold
+    global current_handlers
+    global prev_parts
+    global prev_types
+    global prev_messages
+    global handlers_on_hold
     global players
-    global Started
-    global not_advancing
-    current_nandler.message = message
+    global not_advancing_flags
+    current_handlers[message.chat.id].message = message
     if message.text == "Вернуться к игре":
-        message = prev_message
-        if prev_message.text == "/start":
-            Started = False
+        message = prev_messages[message.chat.id]
+        if prev_messages[message.chat.id].text == "/start":
             send_welcome(prev_message)
-        current_nandler = handler_on_hold
-        if prev_message.text == "Transition":
-            current_nandler.handle_start()
+        current_handlers[message.chat.id] = handlers_on_hold[message.chat.id]
+        if prev_messages[message.chat.id].text == "Transition":
+            current_handlers[message.chat.id].handle_start()
             return
-        if prev_type == "Scene":
-            not_advancing = False
+        if prev_types[message.chat.id] == "Scene":
+            not_advancing_flags[message.chat.id] = False
             players[message.chat.id].part_type = "Scene"
-            current_nandler.player.part_type = "Scene"
-            current_nandler.player.current_part = prev_part
-            current_nandler.message = prev_message
-            current_nandler.handle_scene()
-            players[message.chat.id] = current_nandler.player
+            current_handlers[message.chat.id].player.part_type = "Scene"
+            current_handlers[message.chat.id].player.current_part = prev_parts[message.chat.id]
+            current_handlers[message.chat.id].message = prev_messages[message.chat.id]
+            current_handlers[message.chat.id].handle_scene()
+            players[message.chat.id] = current_handlers[message.chat.id].player
             return
         else:
-            not_advancing = False
+            not_advancing_flags[message.chat.id] = False
             players[message.chat.id].part_type = "Task"
-            current_nandler.player.part_type = "Task"
-            current_nandler.player.current_part = prev_part
-            current_nandler.message = prev_message
-            current_nandler.handle_task()
-            players[message.chat.id] = current_nandler.player
+            current_handlers[message.chat.id].player.part_type = "Task"
+            current_handlers[message.chat.id].player.current_part = prev_parts[message.chat.id]
+            current_handlers[message.chat.id].message = prev_messages[message.chat.id]
+            current_handlers[message.chat.id].handle_task()
+            players[message.chat.id] = current_handlers[message.chat.id].player
             return
-    new_player = current_nandler.handle_theory()
+    new_player = current_handlers[message.chat.id].handle_theory()
     players[message.chat.id] = new_player
 
 @bot.message_handler(func=lambda message: message.text == "Теория")
 def theory(message):
-    global handler_on_hold
+    global handlers_on_hold
     global players
-    global current_nandler
-    type_on_hold = players[message.chat.id].part_type
-    handler_on_hold = current_nandler
-    current_nandler = Theory_Handler.Theory_Handler(bot, message)
+    global current_handlers
+    handlers_on_hold[message.chat.id] = current_handlers[message.chat.id]
+    current_handlers[message.chat.id] = Theory_Handler.Theory_Handler(bot, message)
     players[message.chat.id].part_type = "Theory"
-    current_nandler.handle_start()
+    current_handlers[message.chat.id].handle_start()
 
 def generate_markup_for_theory(answers):
     markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
@@ -113,57 +114,53 @@ def get_ids(message):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    global Started
     global players
-    global current_nandler
-    global prev_type
-    global prev_part
-    global prev_message
-    text_directory = os.getcwd() + "/JsonScenes"
-    if Started == False:
-        Started = True
-        current_nandler = Level_1.Level_1_Handler(message.chat.id, message, bot)
-        players[message.chat.id] = current_nandler.player
-        current_nandler.handle_start()
-        prev_message = message
-        prev_type = players[message.chat.id].part_type
-        prev_part = players[message.chat.id].current_part
-    else:
-        bot.send_message(message.chat.id, "Твоё приключение уже началось")
+    global current_handlers
+    global prev_types
+    global prev_parts
+    global prev_messages
+    global not_advancing_flags
+    current_handlers[message.chat.id] = Level_1.Level_1_Handler(message.chat.id, message, bot)
+    players[message.chat.id] = current_handlers[message.chat.id].player
+    current_handlers[message.chat.id].handle_start()
+    prev_messages[message.chat.id] = message
+    prev_types[message.chat.id] = players[message.chat.id].part_type
+    prev_parts[message.chat.id] = players[message.chat.id].current_part
+    not_advancing_flags[message.chat.id] = False
 
 @bot.message_handler(commands=["Level1", "Level2", "Level3", "Level4", "Level5", "Level6"])
 def go_to_level(message):
-    global  current_nandler
+    global current_handlers
     if message.text == "/Level1":
-        current_nandler = Level_1.Level_1_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_1.Level_1_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
     elif message.text == "/Level2":
-        current_nandler = Level_2.Level_2_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_2.Level_2_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
     elif message.text == "/Level3":
-        current_nandler = Level_3.Level_3_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_3.Level_3_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
     elif message.text == "/Level4":
-        current_nandler = Level_4.Level_4_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_4.Level_4_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
     elif message.text == "/Level5":
-        current_nandler = Level_5.Level_5_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_5.Level_5_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
     elif message.text == "/Level6":
-        current_nandler = Level_6.Level_6_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        players[message.chat.id] = current_nandler.player
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_6.Level_6_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        players[message.chat.id] = current_handlers[message.chat.id].player
+        prev_messages[message.chat.id].text = "Transition"
 
 
 def check_player_in_dict(id, type):
@@ -173,102 +170,103 @@ def check_player_in_dict(id, type):
 
 @bot.message_handler(func=lambda message: check_player_in_dict(message.chat.id, "Scene"), content_types=['text'])
 def handle_scene(message):
-    global current_nandler
-    global prev_part
-    global prev_message
-    global prev_type
-    global not_advancing
+    global current_handlers
+    global prev_parts
+    global prev_messages
+    global prev_types
+    global not_advancing_flags
     #Менять сообщение сцены только если поменялся тип или номер данного куска
 
     #Менять при смене хэндлера вручную все prev (костыль)
     player = players[message.chat.id]
-    if not not_advancing and player.current_part.check_advancing(message.text):
-        prev_type = players[message.chat.id].part_type
-        prev_part = players[message.chat.id].current_part
-        prev_message = message
+    if not not_advancing_flags[message.chat.id] and player.current_part.check_advancing(message.text):
+        prev_types[message.chat.id] = players[message.chat.id].part_type
+        prev_parts[message.chat.id] = players[message.chat.id].current_part
+        prev_messages[message.chat.id] = message
     else:
-        not_advancing = True
-    current_nandler.message = message
-    new_player, transition = current_nandler.handle_scene()
+        not_advancing_flags[message.chat.id] = True
+    current_handlers[message.chat.id].message = message
+    new_player, transition = current_handlers[message.chat.id].handle_scene()
     players[message.chat.id] = new_player
     if transition == 2:
-        current_nandler = Level_2.Level_2_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_2.Level_2_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id].text = "Transition"
     elif transition == 3:
-        current_nandler = Level_3.Level_3_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_3.Level_3_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id].text = "Transition"
     elif transition == 4:
-        current_nandler = Level_4.Level_4_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_4.Level_4_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id].text = "Transition"
     elif transition == 5:
-        current_nandler = Level_5.Level_5_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_5.Level_5_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id].text = "Transition"
     elif transition == 6:
-        current_nandler = Level_6.Level_6_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_6.Level_6_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id].text = "Transition"
 
 @bot.message_handler(func=lambda message: check_player_in_dict(message.chat.id, "Task"), content_types=["text"])
 def handle_task(message):
-    global current_nandler
-    global prev_part
-    global prev_message
-    global prev_type
-    global not_advancing
+    global current_handlers
+    global prev_parts
+    global prev_messages
+    global prev_types
+    global not_advancing_flags
     player = players[message.chat.id]
-    if not not_advancing and player.current_part.check_advancing(message.text):
-        prev_type = players[message.chat.id].part_type
-        prev_part = players[message.chat.id].current_part
-        prev_message = message
+    if not not_advancing_flags[message.chat.id] and player.current_part.check_advancing(message.text):
+        prev_types[message.chat.id] = players[message.chat.id].part_type
+        prev_parts[message.chat.id] = players[message.chat.id].current_part
+        prev_messages[message.chat.id] = message
     else:
-        not_advancing = True
-    current_nandler.message = message
-    new_player, transition = current_nandler.handle_task()
+        not_advancing_flags[message.chat.id] = True
+    current_handlers[message.chat.id].message = message
+    new_player, transition = current_handlers[message.chat.id].handle_task()
     players[message.chat.id] = new_player
     if transition == 2:
-        current_nandler = Level_2.Level_2_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message = "Transition"
+        current_handlers[message.chat.id] = Level_2.Level_2_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id] = "Transition"
     elif transition == 3:
-        current_nandler = Level_3.Level_3_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
+        current_handlers[message.chat.id] = Level_3.Level_3_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id] = "Transition"
         prev_message.text = "Transition"
     elif transition == 4:
-        current_nandler = Level_4.Level_4_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_4.Level_4_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id] = "Transition"
     elif transition == 5:
-        current_nandler = Level_5.Level_5_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_5.Level_5_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id] = "Transition"
     elif transition == 6:
-        current_nandler = Level_6.Level_6_Handler(message.chat.id, message, bot)
-        current_nandler.handle_start()
-        prev_part = current_nandler.player.current_part
-        prev_type = current_nandler.player.part_type
-        prev_message.text = "Transition"
+        current_handlers[message.chat.id] = Level_6.Level_6_Handler(message.chat.id, message, bot)
+        current_handlers[message.chat.id].handle_start()
+        prev_parts[message.chat.id] = current_handlers[message.chat.id].player.current_part
+        prev_types[message.chat.id] = current_handlers[message.chat.id].player.part_type
+        prev_messages[message.chat.id] = "Transition"
 
 
 # @server.route('/' + config.Token, methods=['POST'])
