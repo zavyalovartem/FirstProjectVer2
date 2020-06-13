845fd6392a073a1a32e8fefd2f03f9dcdf459dd7
Meh <zavyalovartem79@gmail.com>
2020-06-13 10:50:18 +0500

    Modified webhook

--- MainBot.py
+++ MainBot.py
@@ -281,3 +281,6 @@ def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://divinely-inspired-project.herokuapp.com/' + config.Token)
     return "!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
