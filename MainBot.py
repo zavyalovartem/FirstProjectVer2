be5f92a21474ad13ff737b72a03dba254c4fac0f
Meh <zavyalovartem79@gmail.com>
2020-06-18 14:41:51 +0500

    Webhook experiments

--- MainBot.py
+++ MainBot.py
@@ -303,8 +303,7 @@ def webhook():
     return "!", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 80)))
 
 
 # bot.polling(none_stop=True)
