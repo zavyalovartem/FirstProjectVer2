05e9444a5d06abd5a166ce5eeef67e53d0f5240a
Meh <zavyalovartem79@gmail.com>
2020-06-19 01:08:26 +0500

    debug disabled

--- MainBot.py
+++ MainBot.py
@@ -312,7 +312,7 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)), debug= True)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
 
 
 # bot.remove_webhook()
